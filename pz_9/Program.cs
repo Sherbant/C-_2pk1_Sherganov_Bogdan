f23f6a4752ba2034ec8105f77836a12ce9a5c15d
Sherbant <sherganov.bogdan@yandex.ru>
2022-10-06 20:58:21 +0500

    test edit

--- pz_9/Program.cs
+++ pz_9/Program.cs
@@ -1,15 +1,92 @@
 ﻿using System;
+using System.Linq;
 
-namespace pz_9
+namespace PZ_9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[][] array2 = new string[5][];
-            Random rnd = new Random();
-            array2[0] = new string[rnd.Next(7, 26)];
-            for
+            Console.WriteLine("\nВАРИАНТ 26\n");
+            Random random = new Random();
+            int size = random.Next(7, 26);
+            string[][] array = new string[5][];
+            int k = 0;
+            Console.WriteLine("1.2. Зубчатый массив");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                array[i] = new string[k = random.Next(7, 26)];
+
+                for (int j = 0; j < k; j++)
+                {
+                    array[i][j] = random.Next(7, 26).ToString();
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("3.Последние элементы строк зуб. массива: ");
+            string[] arrayEnd = new string[5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                arrayEnd[i] = array[i][array[i].Length - 1];
+                Console.Write(arrayEnd[i] + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("4.Максимальные элементы зуб. массива: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write((arrayEnd[i] = array[i].Max() + " "));
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("5.Зуб. массив, где первый элемент и максимальный поменялись местами: ");
+            string firstValue = "";
+            string maxValue = "";
+            for (int i = 0, j = 0; i < array.GetLength(0); i++)
+            {
+                maxValue = array[i].Max();
+                int index = Array.IndexOf(array[i], maxValue);
+                firstValue = array[i][j];
+                array[i][j] = array[i].Max();
+                array[i][index] = firstValue;
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("6.Перевернутый зуб. массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = array[i].Length - 1; j >= 0; j--)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("(b)Колличество элементов в каждой строке: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write($"{i + 1}:{array[i].Length}  ");
+            }
+            Console.WriteLine();
+
         }
     }
 }
