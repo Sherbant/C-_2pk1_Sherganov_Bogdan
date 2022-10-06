945d49c31cd2c78f000b1258f7abf2ea08c691af
Богдан Шерганов <bsherganov@gmail.com>
2022-10-06 08:01:00 +0300

    pz_9

---
+++ pz_9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] array2 = new string[5][];
+            Random rnd = new Random();
+            array2[0] = new string[rnd.Next(7, 26)];
+            for
+        }
+    }
+}
