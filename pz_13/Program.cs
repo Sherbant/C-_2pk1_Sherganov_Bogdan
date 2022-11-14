a3db57a6d8fb918c8c0f7238659f34ea1bafa2cd
sherganov.bb2340 <sherganov.bb2340@M409C-1L>
2022-11-14 12:10:40 +0500

    Практическое задание

---
+++ pz_13/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pz_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rul_lenght = 7; /* Длина рулона */
+            double rul_width = 1; /* Ширина рулона */
+            double wall_height = 3; /* Высота стены */
+            double room_width = 4; /* Ширина комнаты */
+            double room_lenght = 6.0; /* Длина комнаты */
+            int rul_count = (int)Math.Ceiling(((room_width + room_lenght) * 2) / ((int)Math.Truncate(rul_lenght / wall_height) * rul_width)); /* Необходимое количество рулонов */
+            double ostatok_percent = 100.0 * (1 - ((room_width + room_lenght) * 2 * wall_height) / (rul_count * rul_lenght * rul_width)); /* Общий остаток в процентах к площади рулонов */
+
+            Console.WriteLine($"Кол во рулонов:{rul_count} , % {ostatok_percent}");
+
+        }
+    }
+}
