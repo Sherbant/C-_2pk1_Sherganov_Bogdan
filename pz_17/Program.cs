5586de58852b1a9f96a3ef504b5828e5be4500c8
sherganov.bb2340 <sherganov.bb2340@M409C-1L>
2023-02-09 11:41:17 +0500

    ddd

---
+++ pz_17/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Random((unsigned)TimeSpan(null));
+            Console.WriteLine("Введите размерность массива:");
+            Console.WriteLine("N = ");
+            Console.ReadLine(N);
+            int A = new int;
+            int ptr = A; //Объявляем указатель на массив
+            Console.WriteLine("Случайный массив [-100, 100]:");
+            for (int i = 0; i < N; i++);
+            {
+                A[i] = -100 + (() % 201); //Заполняем массив случайными числами
+                Console.WriteLine(ptr + i)" "; //Выводим массив с помощью указателя
+            }
+            
+        }
+    }
+}
