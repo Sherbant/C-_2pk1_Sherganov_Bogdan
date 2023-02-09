20bdb6f59a8158113d3e330c4c746245bf448126
Sherbant <sherganov.bogdan@yandex.ru>
2023-02-09 17:06:07 +0500

    Практическое задание

---
+++ pz_15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("input.txt"))
+            {
+                Console.WriteLine("Файл input.txt не существует");
+                Environment.Exit(1);
+            }
+
+            string[] lines = new string[0];
+
+            try
+            {
+                lines = File.ReadAllLines("input.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при чтении: ", e);
+                Environment.Exit(1);
+            }
+
+            foreach (string line in lines)
+            {
+                if (line[0] == line[line.Length - 1])
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
